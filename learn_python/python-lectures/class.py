04a32af7e9ae9bc6f928bef5997a37815e11a73f
Fatima naeem <Fatimanaeem6674@gmail.com>
2024-09-12 07:16:41 -0700

    changes done in the files

--- learn_python/python-lectures/class.py
+++ learn_python/python-lectures/class.py
@@ -1,4 +1,7 @@
-import sys
-print("line1")
-print("line2")
-print(sys.argv)
+# import sys
+# print("line1")
+# print("line2")
+# print(sys.argv)
+with open("./abc.txt") as file:
+    print(type(file))
+
